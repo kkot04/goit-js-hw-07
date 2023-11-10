8de9fc10749d634f0239769ee4cda1811b622002
kkot04 <144848298+kkot04@users.noreply.github.com>
2023-11-10 22:07:42 +0200

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const arrImg = galleryItems.map((item) =>{
+    return `
+    <li class ="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
+        </a>
+    </li>`
+})
+gallery.insertAdjacentHTML('beforeend', arrImg.join(""))
+
+gallery.addEventListener("click", event =>{
+    event.preventDefault();
+    if(event.target.classList.contains('gallery__image')){
+        basicLightbox.create(`<img class='img-generate-big' width="1400" height="900" src="${target.getAttribute("data-source")}" ></img>`)
+    }
+})
+
+// console.log(galleryItems);
